e0ddf9dbbf732a35b562f40e7734848dc825a423
SarthakKahaliya <64351234+SarthakKahaliya@users.noreply.github.com>
2022-05-01 22:34:31 -0400

    Updated footer links

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
 <footer class="navbar fixed-bottom container d-flex mt-auto py-3 bg-dark text-white" style="justify-content: space-between; border-radius: 5px 5px 0px 0px;">
-	<div>Developed Sarthak Kahaliya</div>
+	<div>Developed by Sarthak Kahaliya</div>
 
 	<div>Copyright 2022 @ Sarthak Kahaliya</div>
 
-	<div><a href="" style="color: white;">Github</div>
+	<div><a href="https://github.com/SarthakKahaliya/task-management-system" style="color: white;">Github</div>
+	
+	<div><a href="https://www.linkedin.com/in/sarthak-kahaliya/" style="color: white;">Linkedin</div>
 
 </footer>
