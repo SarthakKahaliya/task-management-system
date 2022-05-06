b0934894fb825c65831d4ba8166c8197ed664e05
SarthakKahaliya <sarthakkahaliya@gmail.com>
2022-05-06 17:59:49 -0400

    fixed logic errors

--- logic/logic.php
+++ logic/logic.php
@@ -7,7 +7,7 @@
 
     // Don't display server errors 
 
-    ini_set("display_errors", "on");
+    ini_set("display_errors", "off");
 
 
     // Initialize a database connection
@@ -19,8 +19,6 @@
 
     if(!$conn){
         echo "<h3 class='container bg-dark p-3 text-center text-warning rounded-lg mt-5'>Not able to establish Database Connection<h3>";
-    }else{
-        echo "success";
     }
 
 
